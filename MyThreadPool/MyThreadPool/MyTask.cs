3427c185b48c4ff139b92ab19c9db475a1a4508a
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-05-07 12:09:33 +0300

    raw thread pool

---
+++ MyThreadPool/MyThreadPool/MyTask.cs
@@ -0,0 +1,49 @@
+namespace MyThreadPool;
+
+public class MyTask<TResult> : IMyTask<TResult>
+{
+    private volatile bool _isCompleted = false;
+    private readonly Func<TResult> _func;
+    private MyThreadPool _threadPool;
+    private TResult? _result;
+    private Action? _continuation { get; set; }
+    public bool IsCompleted => _isCompleted;
+    public TResult? Result => _result;
+    
+    public MyTask (Func<TResult> func, MyThreadPool threadPool)
+    {
+        _func = func ?? throw new ArgumentNullException(nameof(func));
+        _threadPool = threadPool;
+    }
+
+    public void Execute()
+    {
+        try
+        {
+            _result = _func();
+            if (_continuation != null) _threadPool.Submit(() => _continuation);
+        }
+        catch (Exception ex)
+        {
+            _result = default;
+            throw new AggregateException(ex);
+        }
+        finally
+        {
+            _isCompleted = true;
+        }
+        
+    }
+
+    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult?, TNewResult> continueFunc)
+    {
+        if (!IsCompleted) throw new Exception("Parent task hasn't been executed yet.");
+        
+        var newFunc = new Func<TNewResult>(() => continueFunc(Result));
+        var newTask = new MyTask<TNewResult>(newFunc, _threadPool);
+
+        _continuation = () => newTask.Execute();
+
+        return newTask;
+    }
+}
